9558c4919c05f74d66ac7badcf15850347c94fb1
sathar123 <satharbest99@gmail.com>
2022-09-19 16:20:44 +0530

    Initial commit

---
+++ bin/demo1.dart
@@ -0,0 +1,11 @@
+import 'dart:io';
+
+
+void main() {
+  String name = 'sathar';
+  int age = 20;
+  double mark =5.5;
+  print('Hello world:');
+  stdout.write('I am $age year old');
+  stdout.write('my mark is $mark');
+}
