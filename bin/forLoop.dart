92e15144cff8825db52d9ec4946c8bb674dcd2cf
sathar123 <satharbest99@gmail.com>
2022-09-26 15:49:32 +0530

    commit

--- bin/forLoop.dart
+++ bin/forLoop.dart
@@ -1,6 +1,11 @@
-void main(){
+import 'dart:io';
 
+void main(){
+  int num;
+  print('Enter a number');
+  num=int.parse(stdin.readLineSync()!);
   for(int i=1;i<=10;i++){
-    print(i);
+    print('$i * $num = ${i*num}');
+
   }
 }
